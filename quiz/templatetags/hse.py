5affb26fd2c25e2918a7ef4d86dea9bba8452515
chapkovski <chapkovski@gmail.com>
2020-11-06 14:49:42 +0300

    duration into rus

--- quiz/templatetags/hse.py
+++ quiz/templatetags/hse.py
@@ -10,12 +10,37 @@ from django import template
 register = template.Library()
 
 
+def pluralize(value, forms):
+    """
+    Подбирает окончание существительному после числа
+    {{someval|pluralize:"товар,товара,товаров"}}
+    https://gist.github.com/dpetukhov/cb82a0f4d04f7373293bdf2f491863c8
+    """
+    try:
+        one, two, many = forms.split(u',')
+        value = str(value)[-2:]  # 314 -> 14
+
+        if (21 > int(value) > 4):
+            return many
+
+        if value.endswith('1'):
+            return one
+        elif value.endswith(('2', '3', '4')):
+            return two
+        else:
+            return many
+
+    except (ValueError, TypeError):
+        return ''
+
+
 @register.filter
 def duration(td):
-    total_seconds = int(td.total_seconds())
-    minutes = total_seconds // 60
-    seconds = td.seconds
-    return '{} минут {} секунд'.format(minutes, seconds)
+    minutes, seconds = divmod(td.total_seconds(), 60)
+    rus_min = pluralize(int(minutes), 'минута,минуты,минут')
+    rus_sec = pluralize(int(seconds), 'секунда,секунды,секунд')
+    return f'{int(minutes)} {rus_min} {int(seconds)} {rus_sec}'
+
 
 @register.filter
 def q_ending(value):
